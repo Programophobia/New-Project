3612a4894ada199223e17f1f73f5ac889bac723a
Anna Powaska <annapowaska@op.pl>
2024-05-16 11:17:19 +0200

    searchForm

--- src/components/searchForm/searchForm.js
+++ src/components/searchForm/searchForm.js
@@ -1,13 +1,26 @@
 import styles from './searcForm.module.scss'
 import TextInput from '../textInput/textInput.js';
 import Button from '../button/button.js';
-
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = (props) => {
+const dispatch = useDispatch();
+const [title, setTitle] = useState('')
+
+
+
+const search = e => {
+    e.preventDefault();
+    dispatch({ type: 'UPDATE_SEARCHSTRING', payload: title });
+    setTitle('');
+   
+}
+
     return (
-        <form className={styles.searchForm}>
-           <TextInput placeholder="search"/>
-           <Button><span className="fa fa-search" /></Button>
+        <form className={styles.searchForm} onClick={search}>
+           <TextInput placeholder="search" value={title}  onChange={e => setTitle(e.target.value)} />
+           <Button  ><span className="fa fa-search" /></Button>
         </form>
     );
   };
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
 
+
+
 const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_COLUMN':
@@ -10,7 +12,10 @@ const reducer = (state, action) => {
       return state;
       case 'ADD_CARD':
         return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
-      }
+      
+      case 'UPDATE_SEARCHSTRING':
+         return { ...state, searchString: action.payload };
+  }
 };
 
 const store = createStore(

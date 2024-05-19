9eccc266bfe9b75bcf45866f240eb7346b13654d
Anna Powaska <annapowaska@op.pl>
2024-05-19 10:05:38 +0200

    Router added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Container from "./components/contener/contener.js";
 import NavBar from "./components/navBar/navBar.js";
 import Home from "./components/home/home.js";
+import Fav from "./components/fav/fav.js";
+import About from "./components/about/about.js";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Container>
           <Home />
+          <Fav />
+          <About />
         </Container>
     </div>
   );

005e0c310d30c7c809dafccae6c300ce03223a65
Anna Powaska <annapowaska@op.pl>
2024-05-19 09:26:35 +0200

    NavBar added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Hero from "./components/hero/hero.js";
 import SearchForm from "./components/searchForm/searchForm.js";
 import List from "./components/list/list.js";
 import Container from "./components/contener/contener.js";
+import NavBar from "./components/navBar/navBar.js";
 
 const App = () => {
   return (
     <div>
-    <Container>
-        <Hero />
-        <SearchForm />
-        <List />
-      </Container>
+      <NavBar />
+      <Container>
+          <Hero />
+          <SearchForm />
+          <List />
+        </Container>
     </div>
   );
 };
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,15 @@
+import styles from './mavBar.module.scss'
+
+const NavBar = (props) => {
+    return (
+        <nav className={styles.navigation}>
+            <a href="/"><i className="fa fa-tasks"></i></a>
+            <ul className={styles.menu}>
+                <li><a href="/">HOME</a></li>
+                <li><a href="/favorite">FAVORITE</a></li>
+                <li><a href="/about">ABOUT</a></li>
+            </ul>
+        </nav>)
+}
+
+export default NavBar

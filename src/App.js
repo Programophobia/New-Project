ca09a284bb778a718c21d77fbb89524d34b62af3
Anna Powaska <annapowaska@op.pl>
2024-05-19 10:16:58 +0200

    Router connected

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import NavBar from "./components/navBar/navBar.js";
 import Home from "./components/home/home.js";
 import Fav from "./components/fav/fav.js";
 import About from "./components/about/about.js";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Container>
-          <Home />
-          <Fav />
-          <About />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/Favorite' element={<Fav />} />
+          <Route path='/about' element={<About />} />
+          </Routes>
         </Container>
     </div>
   );

7bed687a632358c46d14bd38d2c6e69e814575f1
Anna Powaska <annapowaska@op.pl>
2024-05-27 07:04:17 +0200

    Navigate

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -5,13 +5,14 @@ import { useSelector } from 'react-redux';
 import { getAllColumns, getListById, getColumnsByList } from '../../redux/store.js';
 import { useParams } from 'react-router';
 import SearchForm from '../searchForm/searchForm.js'
+import { Navigate } from 'react-router-dom';
 
 const List = (props) => {
   const { listId } = useParams();
 
   const listData = useSelector(state => getListById(state, listId))
   const columns = useSelector(state => getColumnsByList(state, listId));
-
+  if(!listData) return <Navigate to="/" />
     return (
             <div className={styles.list}>
               <header className={styles.header}>
